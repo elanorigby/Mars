0e08eadf77e514933a1aacd5b9dfdb0b3a9ea6d0
Sharlak <elanorigby@gmail.com>
2017-08-04 19:49:11 +0100

    test brainstorm

---
+++ _test.py
@@ -0,0 +1,19 @@
+# Input tests:
+#   Is the first line coordinates?
+#       two numbers separated by a space
+#   Are all the characters received expected characters?
+#       must be in ('0123456789NSEWLRM ')
+#   Did all the rovers land on the plateau?
+#       roverX between 0 and inputX
+#       roverY between 0 and inputY
+
+# Method tests:
+#   turnR(direction) -> correct new direction
+#   turnL(direction) -> correct new direction
+#   move(direction, coords) -> correct new coords
+
+
+# Output tests:
+#   Did all the rovers stay on the plateau?
+#       roverX between 0 and inputX
+#       roverY between 0 and inputY
