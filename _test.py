3dd1cadd97b273679e9d72d8f7949c7573312403
Sharlak <elanorigby@gmail.com>
2017-08-08 11:12:49 +0100

    file open test

--- _test.py
+++ _test.py
@@ -1,19 +1,22 @@
-# Input tests:
-#   Is the first line coordinates?
-#       two numbers separated by a space
+import roving
+
+file1 = 'instructions/file1.txt'
+
+def test_file_opener():
+    assert roving.opener(file1) != None
+
+
 #   Are all the characters received expected characters?
 #       must be in ('0123456789NSEWLRM ')
-#   Did all the rovers land on the plateau?
-#       roverX between 0 and inputX
-#       roverY between 0 and inputY
 
-# Method tests:
+#   Did all the rovers land on the plateau?
+#       For each rover:
+#           roverX between 0 and inputX
+#           roverY between 0 and inputY
 #   turnR(direction) -> correct new direction
 #   turnL(direction) -> correct new direction
 #   move(direction, coords) -> correct new coords
 
-
-# Output tests:
 #   Did all the rovers stay on the plateau?
 #       roverX between 0 and inputX
 #       roverY between 0 and inputY
