5703c9aa794e0529d956aa608474a51fbbcbdfd9
Sharlak <elanorigby@gmail.com>
2017-08-10 07:27:52 +0100

    added test for whole applicataion flow

--- tests/test_report.py
+++ tests/test_report.py
@@ -14,3 +14,20 @@ def test_report(start, moves, grid, expected):
     rover = roving.Rover(start, moves)
     report = roving.Report(plateau, rover)
     assert report.message() == expected
+
+
+@pytest.mark.parametrize('file, expected', [
+    ('instructions/file1.txt', [(1, 3, 'N'), (5, 1, 'E')]),
+    ('instructions/file2.txt', [(6, 30, 'W'), (21, 21, 'E'), (33, 26, 'E'), (10, 17, 'E')]),
+    ('instructions/file3.txt', ['Woops! This rover fell off the plateau. Nice driving, NASA.', (3, 8, 'S')]),
+    ])
+def test_total(file, expected):
+    control = roving.Control(file)
+    plateau = control.makegrid()
+    roverlist = control.makerovers()
+    reportlist = []
+    for rover in roverlist:
+        rover.makeitso()
+        report = roving.Report(plateau, rover)
+        reportlist.append(report.message())
+    assert reportlist == expected
