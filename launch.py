c63382775a64475361d860e1aa4dbde788e9103e
Sharlak <elanorigby@gmail.com>
2017-08-10 12:12:21 +0100

    moved script outside of package of modules because apparantly that's a thing

---
+++ launch.py
@@ -0,0 +1,15 @@
+from .control import Control
+from .report import Report
+
+import sys
+
+
+if __name__ == '__main__':
+    file = sys.argv[1]
+    con = Control(file)
+    plat = con.makeplateau()
+    roverlist = con.makerovers()
+    for rov in roverlist:
+        rov.makeitso()
+        rep = Report(plat, rov)
+        print(rep.message())
