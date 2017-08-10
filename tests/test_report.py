180a677ae9798b1e68f46f43ad4171de689a2cd6
Sharlak <elanorigby@gmail.com>
2017-08-10 10:16:09 +0100

    test for ongrid method

--- tests/test_report.py
+++ tests/test_report.py
@@ -2,8 +2,16 @@ import pytest
 
 from ..mission import roving
 
+@pytest.mark.parametrize('start, moves, grid, expected', [
+    ('3 3 E', 'MM', '14 14', True),
+    ('34 12 N', 'MM', '45 10', False)
+    ])
+def test_ongrid(start, moves, grid, expected):
+    plateau = roving.Plateau(grid)
+    rover = roving.Rover(start, moves)
+    report = roving.Report(plateau, rover)
+    assert report.ongrid() == expected
 
-########## Report Class Tests ##########
 
 @pytest.mark.parametrize('start, moves, grid, expected', [
     ('3 3 E', 'MM', '14 14', (3, 3, 'E')),
